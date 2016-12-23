511d140d85945b4490048d4b38f1415bffa37cce
Haotian <liuhaotian0520@163.com>
2016-12-23 09:48:02 +0800

    Initialize the TIC, time-crucial process should be done here.

--- main.ino
+++ main.ino
@@ -1,7 +1,19 @@
-void setup(){
-	// initialize here.
+int resetClock = 65284; // 65536 - 65535 / 1048 * 4
+void setup() {
+  // set pinMode.
+
+  // start TIC.
+  TCCR1A = 0;
+  TCCR1B = 1<<CS12;
+  TCNT1 = resetClock;
+  bitSet(TIMSK1, TOIE1);
 }
 
-void loop(){
-	// loop here.
+void loop() {
+}
+
+ISR(TIMER1_OVF_vect){
+  // Things done here should not cause too many delay.
+  show47digit(); // this should be put here for continuous refresh
+  TCNT1 = resetClock;
 }
